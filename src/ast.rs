5313500899a3fc8cbd74e1b31e992ebf61042bfd
asobininn <asobininn2000@gmail.com>
2026-07-28 01:51:33 +0900

    feat: AST

---
+++ src/ast.rs
@@ -0,0 +1,52 @@
+use std::{borrow::Cow, ops::Range};
+
+use la_arena::{Arena, Idx};
+
+pub type Span = Range<u32>;
+pub type DirectiveId<'s> = Idx<Directive<'s>>;
+
+#[derive(Debug, Clone)]
+pub struct Directive<'s> {
+    pub name: &'s str,
+    pub name_span: Span,
+    pub args: Vec<Arg<'s>>,
+    pub kind: DirectiveKind<'s>,
+    pub parent: Option<DirectiveId<'s>>,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub struct Arg<'s> {
+    pub value: Cow<'s, str>,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub enum DirectiveKind<'s> {
+    Simple {
+        semicolon_span: Span,
+    },
+    Block {
+        directives: Vec<DirectiveId<'s>>,
+        open_brace_span: Span,
+        close_brace_span: Span,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub struct ConfigAst<'s> {
+    pub directives: Arena<Directive<'s>>,
+    pub roots: Vec<DirectiveId<'s>>,
+}
+
+impl<'s> ConfigAst<'s> {
+    // パンくずを作るのに使う
+    pub fn ancestors(&self, directive: DirectiveId<'s>) -> impl Iterator<Item = DirectiveId<'s>> {
+        let mut current = self.directives[directive].parent;
+        std::iter::from_fn(move || {
+            let next = current;
+            current = next.and_then(|id| self.directives[id].parent);
+            next
+        })
+    }
+}
